35e10e4362da14dcddba124aa321fb26ed1fc822
Raquel Franco <raquelfsobral@gmail.com>
2024-03-02 11:07:58 -0300

    iniciando

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Obter a lista de espécies de Pokémons
+        var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
+        var request = new RestRequest(Method.GET);
+        IRestResponse response = client.Execute(request);
+    }
+}
