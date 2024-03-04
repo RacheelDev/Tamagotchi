53783e53724838e8ea404d073b2abdda6dafe1ef
Raquel Franco <raquelfsobral@gmail.com>
2024-03-04 08:57:03 -0300

    Revert "adicionando comentário teste"
    This reverts commit 7aee44e6fc0943157020839572eb6f4f7a8fc133.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Obter a lista de espécies de Pokémons 
-        // teste
+        // Obter a lista de espécies de Pokémons
         var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
         var request = new RestRequest(Method.GET);
         IRestResponse response = client.Execute(request);
