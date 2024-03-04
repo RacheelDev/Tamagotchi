7aee44e6fc0943157020839572eb6f4f7a8fc133
Raquel Franco <raquelfsobral@gmail.com>
2024-03-04 08:31:01 -0300

    adicionando comentário teste

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Obter a lista de espécies de Pokémons
+        // Obter a lista de espécies de Pokémons 
+        // teste
         var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
         var request = new RestRequest(Method.GET);
         IRestResponse response = client.Execute(request);
